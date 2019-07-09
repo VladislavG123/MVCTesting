fc2bfd70af5ce9a067a9ff84a50116170b2cbbc3
Владислав Горохов <gbg.0101@mail.ru>
2019-07-09 16:53:09 +0600

    Adde new controler, view and model

---
+++ TestingMVC/Models/Movie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestingMVC.Models
+{
+    public class Movie
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public string Title { get; set; }
+
+        public string Genre { get; set; }
+
+        public int Duration { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
